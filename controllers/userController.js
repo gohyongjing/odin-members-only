462122eb1fbac90492f987f9e6fe073b4f03f564
Yong Jing <gohyongjing@gmail.com>
2022-12-30 20:37:58 +0800

    Add home page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,26 @@
+const async = require("async");
+
+const Message = require("../models/message");
 const User = require("../models/user");
 
-exports.index = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+exports.index = (req, res) => {
+  async.parallel(
+    {
+      user_count(callback) {
+        User.countDocuments({}, callback);
+      },
+      message_count(callback) {
+        Message.countDocuments({}, callback);
+      },
+    },
+    (err, results) => {
+      res.render("index", {
+        title: "Members Only Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 exports.user_create_get = (req, res, next) => {
